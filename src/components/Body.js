075907b8b3b78de3f38fe4677fa0f174328cdfc3
JinwangMok <wlsdhkd8040@gmail.com>
2021-05-26 15:41:34 +0900

    build game

--- src/components/Body.js
+++ src/components/Body.js
@@ -51,30 +51,27 @@ const contentsData = [//전달받아야하는 데이터2
         id : 1,
         subject : "음식",
         title : '든든한 국밥 월드컵',
+        roundOf : 4,//4강
         item : [
             {
                 id : 1,
                 name : "돼지국밥",
                 img : 'http://placeimg.com/64/64/1',
-                count : 0
             },
             {   
                 id : 2,
                 name : "순대국밥",
                 img : 'http://placeimg.com/64/64/2',
-                count : 0
             },
             {
                 id : 3,
                 name : "굴국밥",
                 img : 'http://placeimg.com/64/64/3',
-                count : 0
             },
             {
                 id : 4,
                 name : "콩나물국밥",
                 img : 'http://placeimg.com/64/64/4',
-                count : 0
             },
         ]
     }, 
@@ -82,30 +79,27 @@ const contentsData = [//전달받아야하는 데이터2
         id : 2,
         subject : "영화",
         title : '썸타는 사람과 같이 보고 싶은 영화 월드컵',
+        roundOf : 4,
         item : [
             {
                 id : 1,
                 name : "러브,로지",
                 img : 'http://placeimg.com/64/64/5',
-                count : 0
             },
             {
                 id : 2,
                 name : "비긴어게인",
                 img : 'http://placeimg.com/64/64/6',
-                count : 0
             },
             {
                 id : 3,
                 name : "부산행",
                 img : 'http://placeimg.com/64/64/7',
-                count : 0
             },
             {
                 id : 4,
                 name : "인터스텔라",
                 img : 'http://placeimg.com/64/64/8',
-                count : 0
             },
         ]
     },
@@ -113,30 +107,27 @@ const contentsData = [//전달받아야하는 데이터2
         id : 3,
         subject : "동물",
         title : '인생에서 한번쯤 키워보고 싶은 동물 월드컵',
+        roundOf : 4,
         item : [
             {
                 id : 1,
                 name : "강아지",
                 img : 'http://placeimg.com/64/64/9',
-                count : 0
             },
             {
                 id : 2,
                 name : "고양이",
                 img : 'http://placeimg.com/64/64/10',
-                count : 0
             },
             {
                 id : 3,
                 name : "사자",
                 img : 'http://placeimg.com/64/64/11',
-                count : 0
             },
             {
                 id : 4,
                 name : "캥거루",
                 img : 'http://placeimg.com/64/64/12',
-                count : 0
             },
         ]
     },
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from 'react';
 
 const Game = ({gameData}) =>{
-    const [lists, setLists] = useState([])
+    const [lists, setLists] = useState([]);
     const [displays, setDisplays] = useState([]);
+    const [winLists, setWinLists] = useState([]);
+    const [round, setRound] = useState(gameData.roundOf);
+    const [set, setSet] = useState(1);
     const items = gameData.item;
-    var roundCount = 0;
-    //해야할 일 -> 게임 기능 구현 -> 선택 후 다음 이미지 렌더링
-    
+    const setNumber = round/2;
+
     useEffect(() => {
         items.sort(() => Math.random() - 0.5);
         setLists(items);
@@ -14,27 +16,40 @@ const Game = ({gameData}) =>{
     },[]);
 
     const chooseList = (list) =>{
-        displays.map(d => {
-            if(d.id === list.id){
-                d.count += 1;
-            }else if (d.id !== list.id){
-                d.count -= 1;
+        if(round/2 === set){
+            setRound(round/2);
+            setSet(1);
+        }else{
+            setSet(set + 1);
+        }
+        if(lists.length <= 2){
+            if(winLists.length === 0){
+                setDisplays([list]);
+            }else{
+                let updateList = [...winLists, list];
+                setLists(updateList);
+                setDisplays([updateList[0], updateList[1]]);
+                setWinLists([]);
             }
-        })
-        items = items.filter(i => i.count === roundCount)
-        setLists(items);
-        console.log(items);
+        }else if(lists.length > 2){
+            setWinLists([...winLists, list]);
+            setDisplays([lists[2], lists[3]]);
+            setLists(lists.slice(2));//2부터 끝까지 추출
+        }
     }
     return(
     <div className="contents">
             <div className="game">
                 <div className="game_title">{gameData.title}</div>
+                <div className="game_roundSet">
+                {round === 2? "결승전" : round === 1? "WINNER" : round + "강 " + set + "세트"}
+                </div>
                 <div className="game_board">
                     {displays.map(d => {
                         return(
                             <div className="game_list" key={d.name} onClick={() => chooseList(d)}>
                                 <div className="game_list_img">
-                                    <img src={d.img} alt="left-image"/>
+                                    <img src={d.img} alt="image"/>
                                 </div>
                                 <div className="game_list_name">
                                     {d.name}
